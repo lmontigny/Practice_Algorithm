d763f38b8b94b4a2e9057ad81e0ae09b9b410098
lmontigny <laurent.montigny@gmail.com>
2019-04-06 15:00:18 +0200

    added quicksort

---
+++ quick_sort.c
@@ -0,0 +1,54 @@
+#include <stdio.h>
+
+void printArray(int A[], int size)
+{
+    int i;
+    for (i=0; i < size; i++) {
+        printf(" %d ", A[i]);
+    }
+    printf("\n");
+}
+
+int partition(int A[], int low, int high)
+{
+    int i,j,p,tmp;
+    p = high;
+    i = low;
+    j = high;
+    while(i<j){
+        // Search for element bigger than pivot to be moved
+        while(A[i]<=A[p] && i<high)
+            i++;
+        // Search for elements smaller than pivot to be moved
+        while(A[j]>A[p])
+            j--;
+        // Swap elements
+        if(i<j){
+            tmp = A[i];
+            A[i] = A[j];
+            A[j] = tmp;
+        }
+    }
+    return p;
+}
+
+void quickSort(int A[], int low, int high)
+{
+    if(low<high){
+        int p = partition(A, low, high);
+        quickSort(A, low, p-1);
+        quickSort(A, p+1, high);
+    }
+}
+
+int main()
+{
+    int A[] = {10, 7, 8, 9, 1, 5};
+    int n = sizeof(A)/sizeof(A[0]);
+
+    printArray(A, n);
+    quickSort(A, 0, n-1);
+    /* printArray(A, n); */
+
+    return 0;
+}
