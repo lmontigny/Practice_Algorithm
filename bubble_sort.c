4ef297967514ed0a5fb4df7a7a5dec4d001e9e98
lmontigny <laurent.montigny@gmail.com>
2019-04-11 20:23:56 +0200

    Added bubble sort

---
+++ bubble_sort.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+
+void swap(int* a, int* b)
+{
+    int tmp = *a;
+    *a = *b;
+    *b = tmp;
+}
+
+void bubbleSort(int *arr, int n)
+{
+    int i,j;
+    for(i=0; i<n-1; i++){
+        for(j=0; j<n-i-1; j++){
+            if(arr[j]>arr[j+1]){
+                swap(&arr[j], &arr[j+1]);
+            }
+        }
+    }
+}
+
+void printArray(int *arr, int n)
+{
+    int i;
+    for(i=0; i<n; i++){
+        printf("%d ", arr[i]);
+    }
+    printf("\n");
+}
+
+int main()
+{
+    int arr[5] = {10,4,11,5,3};
+    int n = sizeof(arr)/sizeof(arr[0]);
+    printArray(arr, n);
+    bubbleSort(arr, n);
+    printArray(arr, n);
+
+    return 0;
+}
